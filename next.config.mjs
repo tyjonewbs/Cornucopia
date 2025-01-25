51e5b368bc3fdb9f1af6bab53d879063839518fb
tyjonewbs <tylerlnewberry@gmail.com>
2025-01-24 16:38:56 -0800

    second fix for ORM

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['@prisma/client'],
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'prisma']
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('@prisma/client')
+    }
+    return config
+  },
   output: 'standalone',
   async headers() {
     return [
